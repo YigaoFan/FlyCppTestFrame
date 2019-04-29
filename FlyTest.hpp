f55421dd6dbc961efaf3b2b4f9d04988ebad4601
YigaoFan <fanyigao@qq.com>
2019-04-29 08:46:26 +0800

    update a little relation between two class

--- FlyTest.hpp
+++ FlyTest.hpp
@@ -9,13 +9,25 @@ class Section {
 	friend class Condition;
 public:
 	Section(Condition& condition);
+	Section(bool state)
+	: _state(state){}
 
 	bool state()
 	{
-		if (_subSections.size() == 0) {
+		if (_subSections.empty()) {
 			return _selfDone;
 		}
 		// else traverse the sub-tree
+		for (auto s : _subSections) {
+			if (!s->state()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool allBranchDone()
+	{
 		for (auto s : _subSections) {
 			if (!s->state()) {
 				return false;
@@ -31,17 +43,12 @@ public:
 private:
 	std::vector<Section*> _subSections;
 	bool _selfDone{ false };
+	bool& _state; // just represent once call end or not
 };
 
 class Condition {
-	// should also know the tree relationship(means share)
 public:
 	Section& correspondSection;
-//	Condition()
-//	: _section(nullptr)
-//	{
-//
-//	}
 
 	Condition(Section& section)
 		: correspondSection(section)
@@ -55,7 +62,7 @@ public:
 	~Condition()
 	{
 		// means it's leaf
-		if (correspondSection._subSections.size() == 0)
+		if (correspondSection._subSections.empty())
 		{
 			correspondSection.markDone();
 		}
@@ -68,21 +75,24 @@ Section::Section(Condition &condition)
 	condition.correspondSection._subSections.emplace_back(this);
 }
 
-static std::vector<std::pair<Condition, std::function<void(Condition&)>>> tests{};
+static std::vector<std::function<void(Condition&)>> _tests{};
 
 class RegisterTestCase {
 public:
 	RegisterTestCase(const std::function<void(Condition&)> testCase) // the const maybe not right
 	{
 		// Here exist a problem
-		tests.emplace_back(std::make_pair(Condition{}, testCase));
+		_tests.emplace_back(testCase);
 	}
 };
 
+// example
 static RegisterTestCase testCase = (std::function<void(Condition&)>)[] (Condition& condition) {
+	// do something
 	std::vector<int> a;
 	a.push_back(1);
-	static Section section{condition}; if (Condition condition = section) {
+
+	static Section section1{ condition }; if (Condition condition = section1) {
 		// How to return?
 	}
 };
@@ -91,9 +101,13 @@ static
 bool
 allTest()
 {
-	for (auto& t : tests) {
-		while (!t.first) { // t is false means t is not complete
-			t.second(t.first);
+	for (auto& t : _tests) {
+		bool state = false;
+		Section testFunc{ state };
+		Condition condition{ testFunc };
+		while (!testFunc.allBranchDone()) { // t is false means t is not complete
+			t(condition);
+			state = false; // call once will set the state to true
 		}
 	}
 }
