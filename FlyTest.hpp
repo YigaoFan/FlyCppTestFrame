b2f32afc59451d5fe6a21a323713a29bcaba1c09
Yigao Fan <v-yifan@microsoft.com>
2019-05-07 19:16:35 +0800

    v1.0

--- FlyTest.hpp
+++ FlyTest.hpp
@@ -7,113 +7,117 @@ using std::runtime_error;
 
 class Condition;
 class Section {
-	friend class Condition;
+    friend class Condition;
 public:
-	Section(Condition& condition);
-	Section(bool& state) : _state(state), _father(nullptr)
-	{}
+    Section(Condition& condition);
+    Section(bool& state) : _state(state), _father(nullptr)
+    {}
 
-	bool state()
-	{
-		if (_state) {
-			// if this call end, no branch need to be run.
-			return true;
-		} else {
-			return allBranchDone();
-		}
-	}
+    bool state()
+    {
+        if (_state) {
+            // if this call end, no branch need to be run.
+            return true;
+        } else {
+            return allBranchDone();
+        }
+    }
 
-	bool allBranchDone()
-	{
-		for (auto s : _subSections) {
-			if (!s->allBranchDone()) {
+    bool allBranchDone()
+    {
+        if (_subSections.empty()) {
+            return _selfDone;
+        }
+
+        for (auto s : _subSections) {
+            if (!s->allBranchDone()) {
 #ifdef FALSE
-				if (_selfDone) {
-					throw runtime_error
-						("When a sub branch is not done, _selfDone of current branch is not be set to false");
-				}
+                if (_selfDone) {
+                    throw runtime_error
+                    ("When a sub branch is not done, _selfDone of current branch is not be set to false");
+                }
 #endif
-				return false;
-			}
-		}
+                return false;
+            }
+        }
 #ifdef FALSE
-		if (_selfDone) {
-			throw runtime_error
-				("When all sub branches are done, _selfDone of current branch is not be set to true");
-		}
+        if (_selfDone) {
+            throw runtime_error
+            ("When all sub branches are done, _selfDone of current branch is not be set to true");
+        }
 #endif
-		return _selfDone; // this maybe have some problem
-	}
+        return true; // this maybe have some problem
+    }
 
-	void markDone()
-	{
-		_selfDone = true;
-//		if (_father != nullptr) {
-//			_father->checkSelfStatus();
-//		}
-	}
+    void markDone()
+    {
+        _selfDone = true;
+        //		if (_father != nullptr) {
+        //			_father->checkSelfStatus();
+        //		}
+    }
 
-	void checkSelfStatus()
-	{
-		auto subStatus{true};
-		for (const auto &s : _subSections) {
-			subStatus &= s->allBranchDone();
-		}
+    void checkSelfStatus()
+    {
+        auto subStatus{ true };
+        for (const auto &s : _subSections) {
+            subStatus &= s->allBranchDone();
+        }
 
-		if (subStatus) {
-			markDone();
-		}
-	}
+        if (subStatus) {
+            markDone();
+        }
+    }
 
-	void turnOffState()
-	{
-		_state = true;
-	}
+    void turnOffState() const
+    {
+        // TODO why this member function can be const
+        _state = true;
+    }
 
-	void turnOnState()
-	{
-		_state = false;
-	}
+    void turnOnState() const
+    {
+        _state = false;
+    }
 
 private:
-	bool _selfDone{ false };
-	bool& _state;
-	Section* const _father;
-	std::vector<Section*> _subSections;
+    bool _selfDone{ false };
+    bool& _state;
+    Section* const _father;
+    std::vector<Section*> _subSections;
 };
 
 class Condition {
 public:
-	Section& correspondSection;
+    Section& correspondSection;
 
-	Condition(Section& section)
-		: correspondSection(section)
-	{}
+    Condition(Section& section)
+        : correspondSection(section)
+    {}
 
-	operator bool()
-	{
-		// ! to let it fit the if condition
-		return !correspondSection.state();
-	}
+    operator bool() const
+    {
+        // ! to let it fit the if condition
+        return !correspondSection.state();
+    }
 
-	~Condition()
-	{
-		// means it's leaf
-		if (correspondSection._subSections.empty())
-		{
-			correspondSection.markDone();
-			correspondSection.turnOffState();
-		}
-	}
+    ~Condition()
+    {
+        // means it's leaf
+        if (correspondSection._subSections.empty() && !correspondSection.state()) {
+            correspondSection.markDone();
+            correspondSection.turnOffState();
+        }
+    }
 
 };
 
-Section::Section(Condition &condition)
-	: _state(condition.correspondSection._state),
-	_father(&condition.correspondSection)
+inline Section::Section(Condition &condition)
+    : _state(condition.correspondSection._state),
+    _father(&condition.correspondSection)
 {
-	// register sub-section
-	condition.correspondSection._subSections.emplace_back(this);
+    // register sub-section
+    condition.correspondSection._subSections.emplace_back(this);
 
 }
 
@@ -121,11 +125,11 @@ static std::vector<std::function<void(Condition&)>> _tests{};
 
 class RegisterTestCase {
 public:
-	RegisterTestCase(std::function<void(Condition&)> testCase) // the const maybe not right
-	{
-		// Here exist a problem
-		_tests.emplace_back(testCase);
-	}
+    RegisterTestCase(std::function<void(Condition&)> testCase) // the const maybe not right
+    {
+        // Here exist a problem
+        _tests.emplace_back(testCase);
+    }
 };
 
 // example
@@ -141,15 +145,15 @@ static
 void
 allTest()
 {
-	for (auto& t : _tests) {
-		auto _funcState = false;
-		Section testFunc{ _funcState };
-		Condition condition{ testFunc };
-		while (!testFunc.allBranchDone()) { // t is false means t is not complete
-			t(condition);
-			testFunc.turnOnState(); // call once will set the _funcState to true
-		}
-	}
+    for (auto& t : _tests) {
+        auto funcState = false;
+        Section testFunc{ funcState };
+        Condition condition{ testFunc };
+        while (!testFunc.allBranchDone()) { // t is false means t is not complete
+            t(condition);
+            testFunc.turnOnState(); // call once will set the _funcState to true
+        }
+    }
 }
 
 #define PRIMITIVE_CAT(A, B) A##B
